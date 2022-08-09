fd3c9b16ca8db5f79e74b1f0b783c2226bbc7040
KASHI TAMANG <tamang.kashi@gmail.com>
2022-08-09 10:23:44 -0700

    passing test 1 stack

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-class Stack {}
+class Stack {
+    #list = [];
+
+    constructor(list){
+      if (list) this.#list = list;
+    }
+
+    push(newItem){
+        //add a new item
+        this.#list.push(newItem);
+    }
+
+    pop(){
+        //remove the last item
+    }
+
+    peek(){
+        //show us the next in line 
+        return this.#list[this.#list.length - 1];
+    }
+}
 
 class Queue {}
 
